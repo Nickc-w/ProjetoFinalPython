743be442253c0d145932988c643a963bddfd06de
Nicolly Candida <nicolly.candida2005@gmail.com>
2024-11-19 17:18:48 -0300

    Update Pedido.py

--- classess/Pedido.py
+++ classess/Pedido.py
@@ -1,3 +1,5 @@
+
+from funcoes.func import *
 class Pedido:
     def __init__(self,nome="",descricao="",precoTotal=0,mesa=""):
         self.nome = nome
@@ -6,17 +8,13 @@ class Pedido:
         self.mesa = mesa
 
     def AdicionarPedido(self):
-        from funcoes.func import SalvarPedido
         SalvarPedido(self.nome, self.descricao, self.precoTotal, self.mesa)
 
     def LerP(self):
-        from funcoes.func import LerPedido
         LerPedido(self.nome)
 
     def Deletar(self):
-        from funcoes.func import ExcluirPedido
         ExcluirPedido(self.nome)
 
     def editar(self):
-        from funcoes.func import editarPedido
         editarPedido(self.nome, self.descricao, self.precoTotal, self.mesa)
